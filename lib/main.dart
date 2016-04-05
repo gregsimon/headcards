abf4a7b5f8c42bac9dbf2a213e56bf20402e2e1f
Greg Simon <gregsimon@gmail.com>
2016-04-05 12:04:09 -0700

    countdown screen addition

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ void main() {
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => new HeadCardsPickCategory(),
         '/play': (BuildContext context) => new HeadCardsGameBoard(),
+        '/getready': (BuildContext context) => new HeadCardsGetReady(),
       }
     )
   );
@@ -66,7 +67,8 @@ class _HeadCardsPickCategoryState
 
   void _playGame() {
     Navigator.push(context, new MaterialPageRoute<Null>(
-      builder: (BuildContext context) => new HeadCardsGameBoard()
+      //builder: (BuildContext context) => new HeadCardsGameBoard()
+      builder: (BuildContext context) => new HeadCardsGetReady()
     ));
   }
 
@@ -88,6 +90,108 @@ class _HeadCardsPickCategoryState
       )
     );
   }
+}
+
+
+// ----------------------------------------------------------------------------
+// HeadCardsGetReady
+//
+// The main game screen.
+//
+class HeadCardsGetReady extends StatefulWidget {
+  HeadCardsGetReady({ Key key}) : super(key: key);
+
+  @override
+  _HeadCardsGetReadyState createState() => new _HeadCardsGetReadyState();
+}
+
+class _HeadCardsGetReadyState
+      extends State<HeadCardsGetReady> 
+      implements mojom.SensorListener {
+
+  static const int kUnknown = 0;
+  static const int kCentered = 1;
+  static const int kPassed = 2;
+  static const int kWon = 3;
+
+  final mojom.SensorServiceProxy _sens = new mojom.SensorServiceProxy.unbound();
+  mojom.SensorListenerStub _stub;
+  Timer _countdownTimer = null;
+  int _counter = 3;
+  int _lastPosition = _HeadCardsGetReadyState.kUnknown;
+  String _msg = "";
+
+  @override
+  initState() {
+    super.initState();
+
+    // Note: the serivcename is ignored, but not be null.
+    shell.connectToService("", _sens);
+    _stub = new mojom.SensorListenerStub.unbound()..impl = this;
+    _sens.ptr.addListener(mojom.SensorType.accelerometer, _stub);
+    _sens.close();
+  }
+
+
+  void change_position(int new_pos) {
+    // debounce the selection
+    //if (new_pos == _lastPosition)
+    //  return;
+
+    _lastPosition = new_pos;
+    if (_lastPosition == _HeadCardsGetReadyState.kCentered) {      
+      setState(() { _msg = null; });
+    } else {
+      _countdownTimer?.cancel();
+      _countdownTimer = null;
+      setState(() { _msg = "Hold device vertical to start!"; });
+    }
+  }
+
+  onAccuracyChanged(int accuracy) {}
+
+  onSensorChanged(mojom.SensorData data) {
+    //print("OnSensorChanged: ${data.values[0]}  ..  ${data.values[1]}  ..  ${data.values[2]}");
+    if (data.values[2] > 5.0)
+      change_position(_GameState.kPassed);
+    else if (data.values[2] <= -9.0)
+      change_position(_GameState.kWon);
+    else
+      change_position(_GameState.kCentered);
+  }
+
+  void countDownTimerFired() {
+    _countdownTimer?.cancel();
+    _countdownTimer = null;
+    _counter--;
+
+    if (0 == _counter) {
+      _stub?.close();
+      Navigator.popAndPushNamed(context, "/play");
+      return;
+    }
+
+    setState(() { _msg = null; });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String msg =_msg;
+    if (_msg == null)
+      msg = "${_counter}";
+
+    if (_countdownTimer == null)
+      _countdownTimer = new Timer(const Duration(seconds: 1), countDownTimerFired);
+
+    return new Scaffold(
+      body: new Center(
+        child: new Text(msg, 
+          style: const TextStyle(color: const Color(0xFF000000), 
+            fontSize: 50.0, fontWeight: FontWeight.bold)
+          )
+        )
+      );
+    }
 }
 
 
@@ -115,7 +219,7 @@ class _GameState
   final mojom.SensorServiceProxy _sens = new mojom.SensorServiceProxy.unbound();
   mojom.SensorListenerStub _stub;
   List<String> _words = <String>['Mickey Mouse', 'Donald Duck', 'Frozone', 'Stitch', 'Baymax'];
-  Timer _wordTimer = null;
+  Timer _gameTimer = null;
   int _wordIndex = 0;
   int _lastPosition = _GameState.kUnknown;
   SoundAssets _sounds = null;
@@ -135,6 +239,8 @@ class _GameState
     _stub = new mojom.SensorListenerStub.unbound()..impl = this;
     _sens.ptr.addListener(mojom.SensorType.accelerometer, _stub);
     _sens.close();
+
+    _gameTimer = new Timer(const Duration(seconds: 10), gameOverTimerFired);    
   }
 
 
@@ -148,10 +254,12 @@ class _GameState
       case _GameState.kWon:
         print("** SUCCESS **");
         _sounds.play('bell');
+        next_word();
         break;
       case _GameState.kPassed:
         print("** PASS **");
         _sounds.play('buzzer');
+        next_word();
         break;
       case _GameState.kCentered:
         print("** Centered **");
@@ -162,32 +270,38 @@ class _GameState
   onAccuracyChanged(int accuracy) {}
 
   onSensorChanged(mojom.SensorData data) {
-    //print("OnSensorChanged: ${data.values[2]}");
-    if (data.values[2] > 8.0)
+    print("OnSensorChanged: ${data.values[0]}  ..  ${data.values[1]}  ..  ${data.values[2]}");
+    if (data.values[2] > 5.0)
       change_position(_GameState.kPassed);
-    if (data.values[2] < -8.0)
+    else if (data.values[2] <= -9.0)
       change_position(_GameState.kWon);
     else
       change_position(_GameState.kCentered);
   }
 
-  void gameOverTimerFired() {
-    _wordTimer = null;
-   
-    // TODO : display score
-    /*
+  void next_word() {
     if ((_wordIndex+1) >= _words.length) {
-      _wordTimer = null;
-      Navigator.pop(context);
-    } else
-      setState(() { _wordIndex++; });*/
+      do_game_over();
+    } else {
+      setState(() {_wordIndex++;});
+    }
+  }
+
+  void do_game_over() {
+
+    // TODO : display score
+    
+    _stub?.close();
+    Navigator.pop(context);
+  }
+
+  void gameOverTimerFired() {
+    _gameTimer = null;
+    do_game_over();   
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_wordTimer == null)
-      _wordTimer = new Timer(const Duration(seconds: 10), gameOverTimerFired);
-
     return new Scaffold(
       body: new Center(
         child: new Text(_words[_wordIndex], 
